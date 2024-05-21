6cd8840c290d7531c68aa39fab55f656f93f3d1d
Arzenaakter <arzenaakter@gmail.com>
2024-05-21 18:01:49 +0600

    initial setup done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { studentRoutes } from './app/modules/student/student.route';
+// import { studentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 // parsers
@@ -8,10 +8,10 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/student', studentRoutes);
+// app.use('/api/v1/student', studentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Server is running!');
 });
 
 export default app;
--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,5 +4,4 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) });
 export default {
   port: process.env.PORT,
   mongodb_url: process.env.MONGODB_URL,
-  salt_Rounds: process.env.saltRounds,
 };
